8c5d5e08e0cbf4402ad4ba414425fe8af2370708
Tyrese <tyrese.k.cardinal@shaw.ca>
2024-05-08 10:41:57 -0600

    finished assignment

--- js/app.js
+++ js/app.js
@@ -31,3 +31,13 @@ elementThree.addEventListener('mouseout', function (event) {
   // Remove the 'hidden' class from the card when the mouse moves away from it
   elementThree.classList.remove('hidden');
 });
+
+// Add a keydown event listener to the document body
+document.body.addEventListener('keydown', function (event) {
+  // Check if the pressed key is the space key (keyCode 32) or (key ' ')
+  if (event.key === ' ') {
+    // Code to be executed when the space key is pressed
+    console.log('The space key was pressed!');
+    // You can perform any action or run any code here
+  }
+});
